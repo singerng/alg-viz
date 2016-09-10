baaf7eed3becb6585c2c6cae38c67b44569bef25
Noah Singer <singerng@gmail.com>
2016-09-09 22:58:19 -0400

    Insertion sort works

--- js/sorts.js
+++ js/sorts.js
@@ -7,7 +7,7 @@ class Sort {
 
         this.list = new VList("Numbers", true, false);
 
-        for (var i = 0; i < 20; i++) this.list.add_object(new VVar("", false, false, Math.round(Math.random() * 500)));
+        for (var i = 0; i < 50; i++) this.list.add_object(new VVar("", false, false, Math.round(Math.random() * 500)));
 
         this.cmps = new VVar("Comparisons", true, true, 0);
         this.xchgs = new VVar("Exchanges", true, true, 0);
@@ -39,7 +39,7 @@ class SelectionSort extends Sort {
             var mindex = i;
 
             for (var j = i; j < this.list.length(); j++) {
-                if (j>0) this.list.uncolor(j-1);
+                if (j > 0) this.list.uncolor(j-1);
                 this.list.color(j, TEST);
 
                 if (this.less(j, mindex)) {
@@ -54,7 +54,6 @@ class SelectionSort extends Sort {
 
             this.xchg(i, mindex);
 
-
             this.list.gap(i);
             if (i > 0) this.list.ungap(i-1);
             this.list.uncolor(mindex);
@@ -63,6 +62,37 @@ class SelectionSort extends Sort {
             i++;
 
             yield;
+        }
+    }
+}
+
+class InsertionSort extends Sort {
+    *execute() {
+        var i = 0;
+        var j;
+
+        while (i < this.list.length()) {
+            if (i > 0) this.list.ungap(i-1);
+            this.list.gap(i);
+            this.list.color(i, TEST);
+            yield;
+
+            j = i;
+
+            while (j != 0 && !this.less(j-1, j)) {
+                this.xchg(j-1, j);
+                this.list.uncolor(j);
+                this.list.color(j-1, TEST);
+                j--;
+
+                yield;
+            }
+
+            this.list.color(j, MIN);
+            yield;
+            this.list.uncolor(j);
+
+            i++;
         }
     }
 }
